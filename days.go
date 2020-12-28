a7471a71cdd9ebca802be5c5615b41f7597063a2
Darrel Herbst <dherbst@gmail.com>
2020-12-28 09:41:06 -0500

    Added struct for the result between now and then.

--- days.go
+++ days.go
@@ -2,6 +2,20 @@ package plan
 
 import "time"
 
+// UntilResult contains the days between now and then.
+type UntilResult struct {
+	// Target is the date you are working to.
+	Target string
+	// Duration is the time.Duration between now and then.
+	Duration time.Duration
+	// Days is the number of calendar days.
+	Days int
+	// WorkingDays is the number of working days between now and then.
+	WorkingDays int
+	// Holidays is the number of Holidays between now and then.
+	Holidays int
+}
+
 // TimeUntil returns the duration until the target date.
 func TimeUntil(targetarg string) (time.Duration, error) {
 	target, err := time.Parse("2006-01-02", targetarg)
